f4c0e01dda71e81e1c7f53c1836b322ef388a0ee
aychar <58487401+hrfarmer@users.noreply.github.com>
2025-05-08 00:49:31 -0500

    add windows support

--- install_firmware.py
+++ install_firmware.py
@@ -1,20 +1,34 @@
-import requests
 import os
-import shutil
 import platform
+import shutil
+import string
+
+import requests
 
 response = requests.get(
-    "https://raw.githubusercontent.com/proveskit/flight_controller_board/main/Firmware/FC_FIRM_v4x_V2.uf2")
+    "https://raw.githubusercontent.com/proveskit/flight_controller_board/main/Firmware/FC_FIRM_v4x_V2.uf2",
+    timeout=30,
+)
 response.raise_for_status()
 
 with open("firmware.uf2", "wb") as f:
     f.write(response.content)
 
 user_os = platform.system()
+
 if user_os == "Darwin":
-    for dir in os.listdir("/Volumes/"):
-        if dir == "RPI-RP2":
+    for directory in os.listdir("/Volumes/"):
+        if directory == "RPI-RP2":
             shutil.move("firmware.uf2", "/Volumes/RPI-RP2")
+
 elif user_os == "Windows":
-    for drive in os.listdrives():
-        print(drive)
+    drives = []
+    for letter in string.ascii_lowercase:
+        if os.path.exists(f"{letter}:\\"):
+            drives.append(f"{letter}:\\")
+
+    for drive in drives:
+        files = os.listdir(drive)
+        if "INDEX.HTM" in files and "INFO_IF2.TXT" in files:
+            # Found PROVES Kit bootloader directory
+            shutil.move("firmware.uf2", drive)
