e11a1e208d081261b8d1f2e28bd28e113b8969f4
Nate Gay <email@nategay.me>
2025-09-01 19:04:48 -0500

    Update

--- src/flight-software/boot.py
+++ src/flight-software/boot.py
@@ -1,38 +1,4 @@
-import os
-import time
+from lib.pysquared.boot.filesystem import mkdir
 
-import storage
-
-mount_points = [
-    "/sd",
-]
-
-wait_time = 0.02
-
-storage.disable_usb_drive()
-print("Disabling USB drive")
-time.sleep(wait_time)
-
-storage.mount("/", False)
-print("Remounting root filesystem")
-time.sleep(wait_time)
-
-attempts = 0
-while attempts < 5:
-    attempts += 1
-    try:
-        for path in mount_points:
-            try:
-                os.mkdir(path)
-                print(f"Mount point {path} created.")
-            except OSError:
-                print(f"Mount point {path} already exists.")
-    except Exception as e:
-        print(f"Error creating mount point {path}: {e}")
-        time.sleep(wait_time)
-        continue
-
-    break
-
-storage.enable_usb_drive()
-print("Enabling USB drive")
+# Create the SD card directory so we can mount it later
+mkdir("/sd")
